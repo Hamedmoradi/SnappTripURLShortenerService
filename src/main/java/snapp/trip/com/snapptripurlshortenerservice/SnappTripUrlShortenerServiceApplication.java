6567b4b401da7faa4e2d861517c5c575ccc0c33d
hamor1369 <hamedmoradi.mailsbox@gmail.com>
2023-04-06 16:07:23 +0330

    first commit

---
+++ src/main/java/snapp/trip/com/snapptripurlshortenerservice/SnappTripUrlShortenerServiceApplication.java
@@ -0,0 +1,13 @@
+package snapp.trip.com.snapptripurlshortenerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnappTripUrlShortenerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnappTripUrlShortenerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/snapp/trip/com/snapptripurlshortenerservice/SnappTripUrlShortenerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package snapp.trip.com.snapptripurlshortenerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnappTripUrlShortenerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
